e8134cb557e36bb794d035c7279b2da9f4eee693
9111965MA <112547702+9111965MA@users.noreply.github.com>
2022-09-19 23:47:38 +0100

    Update app.py

--- Final_Project_DivOps/app.py
+++ Final_Project_DivOps/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , redirect ,render_template, request, 
+from flask import Flask , redirect ,render_template, request 
 from flask_sqlalchemy import SQLAlchemy
  
 db =SQLAlchemy()
